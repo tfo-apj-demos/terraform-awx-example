b40043b77ebb9dd14b1a6e4d25e6c61a8ae7028c
Aaron Evans <aarone@outlook.com.au>
2024-10-25 07:30:57 +1100

    Remove default value for job_template_id variable in variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,6 @@ variable "TFC_WORKSPACE_ID" {}
 variable "job_template_id" {
   description = "The ID of the job template to run."
   type        = string
-  default = "9"
 }
 
 variable "vm_config" {
